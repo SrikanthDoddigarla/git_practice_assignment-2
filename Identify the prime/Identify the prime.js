93eee65a4870b86efd7490ad766a939050bee8d7
SrikanthDoddigarla <115464639+SrikanthDoddigarla@users.noreply.github.com>
2022-10-19 22:36:07 +0530

    identify the prime

---
+++ Identify the prime/Identify the prime.js
@@ -0,0 +1,11 @@
+let num=13;
+ count=0
+    for (i=1;i<=num;i++){
+        if(num%i!=0){
+            count++;
+            
+        }
+        
+    }
+    count==2 ?
+    console.log("Yes") :console.log("No");
